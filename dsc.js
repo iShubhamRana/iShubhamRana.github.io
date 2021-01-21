5fc1f7edc41f855181c1e409824d94ef54561f7f
Shubham <rana2001shubham@gmail.com>
2021-01-21 23:14:05 +0530

    typing effect in hobbies and skills

--- dsc.js
+++ dsc.js
@@ -4,29 +4,57 @@ var nameit = document.getElementById("name");
  var string2="My Hobbies";
  var hobbies=document.getElementById("hobbies");
   
-function printName ( i){
-  if (i==0 ) nameit.innerHTML="S";
-  
-  if (i<string.length){
-    nameit.innerHTML += string[i];
-    i++;
-    setTimeout(printName , speed , i);
+ var string3="My Skills";
+ var skills=document.getElementById("skills");
+
+
+ 
+
+ 
+function home(){
+  function printName ( i){
+    if (i==0 ) nameit.innerHTML="S";
+    
+    if (i<string.length){
+      nameit.innerHTML += string[i];
+      i++;
+      setTimeout(printName , speed , i);
+    }
+   
   }
+  clearInterval();
+  printName(0);
+  setInterval(printName,5000,0);
  
 }
-
- function printHobbies(l){
-  if (l==0 ) hobbies.innerHTML="&#128073";
-  
-  if (l<string2.length){
-    hobbies.innerHTML += string2[l];
-    l++;
-    setTimeout(printHobbies, speed , l);
+ function f(){
+  function printHobbies(l){
+    if (l==0 ) hobbies.innerHTML="&#128073";
+    
+    if (l<string2.length){
+      hobbies.innerHTML += string2[l];
+      l++;
+      setTimeout(printHobbies, speed , l);
+    }
   }
-}
-printName(0);
-setInterval(printName,5000 , 0);
+  clearInterval(); 
+  printHobbies(0);
+  setInterval(printHobbies,5000,0);
+  
+ }
 
+function f2(){
+  function printSkills(l){
+    if (l==0) skills.innerHTML = "&#128073";
+    if( l<string3.length ) 
+    skills.innerHTML += string3[l];
+    l++;
+    setTimeout(printSkills , speed , l);
+  }
+  clearInterval();
+  printSkills(0);
+  setInterval(printSkills,5000 , 0);
+}
 
 
 
